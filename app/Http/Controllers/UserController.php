e3f817d8a5d644d6b0c888541b0e74ff847017ef
MatheusFelipeMarinho <matheus_fellip@hotmail.com>
2022-09-23 22:18:28 -0300

    feat: :zap: second commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // Fazer os filtros
+
+        $user = User::paginate();
+
+        return response()->json($user);
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // Validar
+
+        $user = new User();
+
+        $user->create($request->all());
+
+        return response()->json($user);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //Validar
+        
+        $user = User::find($id);
+
+        return response()->json($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //$id
+
+        $user->update($request->all());
+
+        return response()->json($user);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::delete($id);
+
+        return response()->json($user);
+    }
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,11 +17,16 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $types = ['administrator', 'singer', 'payer', 'freemium'];
+
+        $typeSelected = fake()->randomElement($types);
+
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'type' => $typeSelected,
+            'password' => 'password', // password
             'remember_token' => Str::random(10),
         ];
     }
